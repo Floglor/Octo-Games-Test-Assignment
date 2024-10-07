13204da1ccacf6150558ff71a74b96d3b61cf74a
IhorZ <floglor033@gmail.com>
2024-10-07 06:59:26 +0300

    Added quest log

---
+++ Assets/Log.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Log : MonoBehaviour
+{
+    [SerializeField] private Toggle _checkBox;
+    [SerializeField] private Image _selectionImage;
+    [SerializeField] private string _questDescription;
+
+    public int ProgressStep;
+    public Toggle TextToggle;
+
+    
+    public void DisableImage()
+    {
+        _selectionImage.gameObject.SetActive(false);
+    }
+    
+    public void EnableImage()
+    {
+        _selectionImage.gameObject.SetActive(true);
+    }
+
+    public string GetDescription()
+    {
+        return _questDescription;
+    }
+    
+    
+}
